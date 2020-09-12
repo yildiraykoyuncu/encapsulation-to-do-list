017bcf00ea330e0f07aae9d752ff9875e0148c59
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2020-09-12 12:56:11 +0200

    bug fix

--- src/init.js
+++ src/init.js
@@ -1,38 +1,5 @@
 import { newApp } from './app.js'
-const addNewTodoListHandler = (event) => {
-    if (event.key !== "Enter") return;
 
-    // Create new todo list
-    const name = event.target.value;
-    const todoList = new TodoList(name);
-
-
-    // push new todo list to the state
-
-    newApp._state.todoLists.push(todoList)
-    console.log(todoList);
-
-    // add new todoList to DOM
-
-    const root = document.getElementById("root");
-    const renderedTodoList = todoList.renderInitialScreen();
-    root.appendChild(renderedTodoList);
-    renderedTodoList.querySelector('input[type="text"]')
-        .addEventListener("keyup", todoList.addTodoHandler);
-    renderedTodoList.querySelector("#toggle-button")
-        .addEventListener("click", todoList.toggleAllHandler);
-
-    const todosView = todoList.renderTodos(todoList.state.todos)
-    todosView.addEventListener("change", todoList.toggleCompletedHandler); // event delegation!
-    todosView.addEventListener("click", todoList.deleteTodoHandler);
-    todosView.addEventListener("dblclick", todoList.editTodoHandler);
-    renderedTodoList.appendChild(todosView)
-
-    //clean input field
-
-    event.target.value = ''
-
-};
 
 window.onload = () => {
     document
